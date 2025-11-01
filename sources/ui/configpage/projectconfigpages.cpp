a5c394646933b79de34d263a518d0c4e1ed00bc8
Emil Osvoll <emil_osvoll@hotmail.com>
2025-11-01 15:36:48 +0100

    rename conflicting filename attribute to projectfile

--- sources/ui/configpage/projectconfigpages.cpp
+++ sources/ui/configpage/projectconfigpages.cpp
@@ -221,7 +221,7 @@ void ProjectMainConfigPage::readValuesFromProject()
 	DiagramContext context = m_project -> projectProperties();
 	
 	// Ensure required custom attributes are always present in the general section
-	QStringList required_attributes = {"customer", "filename", "projectno", "projectrev"};
+	QStringList required_attributes = {"customer", "projectfile", "projectno", "projectrev"};
 	bool context_modified = false;
 	for (const QString &attr : required_attributes) {
 		if (!context.contains(attr)) {
